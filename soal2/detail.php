af0550cb77cb782f26e49b3da0662b26889f391c
JepriAlber <jeprialber@gmail.com>
2021-09-22 15:13:52 +0700

    update terbaru

--- soal2/detail.php
+++ soal2/detail.php
@@ -23,7 +23,11 @@ $employees  = $crud->getData('employee','employee_id',$employeeId);
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
-
+    <style>
+      body {
+              background-color: powderblue;
+          }
+    </style>
     <title>Soal Satu</title>
   </head>
   <body>
